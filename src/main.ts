68bf7db87bbf3ad6d84468cc61e4516b08f38d04
mast1ff <mast1ff929@gmail.com>
2025-04-11 22:51:11 +0900

    fix bookmark manage

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { Hono } from "hono";
+import { getCookie, setCookie } from "hono/cookie";
 
 const app = new Hono<{
 	Bindings: CloudflareBindings;
 	Variables: {
-		sessionToken: string;
+		bookmark: string;
 		dbWithReplica: D1DatabaseSession;
 		dbWithoutReplica: D1Database;
 	};
 }>();
 
 app.use((c, next) => {
-	const token = c.req.header("x-d1-token") ?? "first-unconditional";
-	c.set("sessionToken", token);
-	c.set("dbWithReplica", c.env.DB_WITH_REPLICA.withSession(token));
+	const bookmark = getCookie(c, "bookmark") || "first-unconstrained";
+	c.set("bookmark", bookmark);
+	c.set("dbWithReplica", c.env.DB_WITH_REPLICA.withSession(bookmark));
 	c.set("dbWithoutReplica", c.env.DB_WITHOUT_REPLICA);
 	return next();
 });
@@ -23,7 +24,13 @@ app.get("/replica", async (c) => {
 	const { results } = await db.prepare("SELECT * FROM orders").all();
 	const end = Date.now();
 	const elapsed = end - start;
-	c.res.headers.set("x-d1-token", c.get("sessionToken"));
+
+	const latestBookmark = db.getBookmark();
+	if (latestBookmark) {
+		setCookie(c, "bookmark", latestBookmark, {
+			maxAge: 60 * 60, // 1 hour
+		});
+	}
 	return c.json({
 		results,
 		elapsed,
@@ -36,7 +43,6 @@ app.get("/no-replica", async (c) => {
 	const { results } = await db.prepare("SELECT * FROM orders").all();
 	const end = Date.now();
 	const elapsed = end - start;
-	c.res.headers.set("x-d1-token", c.get("sessionToken"));
 	return c.json({
 		results,
 		elapsed,
